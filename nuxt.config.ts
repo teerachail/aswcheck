a5fc6525eb5a9f547f902d02c82935a507ead2ac
Teerachai Laothong <teerachai@perfenterprise.com>
2025-03-16 13:51:45 +0700

    feat: enhance security configuration with comprehensive content security policy and CSRF protection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,10 +29,43 @@ export default defineNuxtConfig({
 
   security: {
     strict: true,
+
     headers: {
       contentSecurityPolicy: {
-        'img-src': ['https://ui-pro-starter.nuxt.dev'],
+        'base-uri': ["'none'"],
+        'default-src': ["'none'"],
+        'connect-src': ["'self'"],
+        'font-src': ["'self'"],
+        'form-action': ["'self'"],
+        'frame-ancestors': ["'self'"],
+        'frame-src': ["'self'"],
+        'img-src': ["'self'", "'https://ui-pro-starter.nuxt.dev'"],
+        'manifest-src': ["'self'"],
+        'media-src': ["'self'"],
+        'object-src': ["'none'"],
+        'script-src-attr': ["'none'"],
+        'style-src': ["'self'", "'nonce-{{nonce}}'"],
+        'script-src': ["'self'", "'strict-dynamic'", "'nonce-{{nonce}}'"],
+        'upgrade-insecure-requests': true,
+        'worker-src': ["'self'"],
       },
-    }
+      crossOriginEmbedderPolicy: 'require-corp',
+      xFrameOptions: 'DENY',
+      xContentTypeOptions: 'nosniff',
+      xPermittedCrossDomainPolicies: 'none',
+      xXSSProtection: '1; mode=block',
+      referrerPolicy: 'same-origin',
+      strictTransportSecurity: {
+        maxAge: 15552000,
+        includeSubdomains: true,
+        preload: true,
+      },
+    },
+
+    csrf: true,
+
+    ssg: {
+      hashStyles: true,
+    },
   },
 })
